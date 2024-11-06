29c42139256b273944c8d97a99396dfb87bf7e91
Silayuth <rockyjimslive@gmail.com>
2024-11-06 10:39:40 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import Owner from './components/Owner'
+
+function App() {
+ 
+
+  return (
+    <Owner />
+  )
+}
+
+export default App
---
+++ src/components/Owner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Owner() {
+  return (
+    <div>
+        <h1>Rocky - Group F - 29</h1>
+        <img src="./images/rocky2.jpg" alt="image" />
+    </div>
+  )
+}
+
+export default Owner
